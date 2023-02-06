766563ab199d946c5e246beab4d5fe32243ab3af
Кибалка Д.А <DMK94@mail.ru>
2023-02-07 02:25:33 +0300

    Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: 
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber > 5)
+{
+    Console.WriteLine("Это выходной");
+}
+else
+{
+    Console.WriteLine("Это рабочий день");
+}
+
